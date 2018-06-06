945570611103bc4972bffe0be5504b3277b37140
RWSdigital <antonio.r@rwsdigital.com>
2018-06-06 18:31:40 +0200

    [UPD] important check added to avoid any problems with incorrect use of the command

--- switch_ssh_key_script.sh
+++ switch_ssh_key_script.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 function switch() {
-  echo "$(cat ~/.ssh/keys/$1)" >| ~/.ssh/id_rsa
-  echo "$(cat ~/.ssh/keys/$1.pub)"  >| ~/.ssh/id_rsa.pub
-  echo "Switched Key to $1"
+	if [ ! -z "$1" ]; then
+	  if [ ! -f ~/.ssh/keys/$1 ]; then
+	  	  echo "No key $1 found!"
+	  else
+		  echo "$(cat ~/.ssh/keys/$1)" >| ~/.ssh/id_rsa
+		  echo "$(cat ~/.ssh/keys/$1.pub)"  >| ~/.ssh/id_rsa.pub
+		  echo "Switched key to $1"
+      fi
+	else
+	  echo "No arguments given! Correct syntax is: switch KEYNAME"
+	fi
 }
 
 function switch-key-add() {
-  ssh-keygen -t rsa -b 4096 -f ~/.ssh/keys/$1
+	if [ ! -z "$1" ]; then
+		if [ ! -f ~/.ssh/keys/$1 ]; then
+			ssh-keygen -t rsa -b 4096 -f ~/.ssh/keys/$1
+		else
+		  echo "Key $1 already exists!"
+		fi
+	else
+	  echo "No arguments given! Correct syntax is: switch-key-add KEYNAME"
+	fi
 }
 
 function switch-key-read() {
-  echo $(cat ~/.ssh/keys/$1.pub)
+	if [ ! -z "$1" ]; then
+  		echo $(cat ~/.ssh/keys/$1.pub)
+	else
+	  echo "No arguments given! Correct syntax is: switch-key-read KEYNAME"
+	fi
 }
